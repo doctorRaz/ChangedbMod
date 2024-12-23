2eab326561621b1cfb81243194e89f28db84c2b3
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2024-12-23 20:42:18 +0300

    add cmdINFO

--- src/CadCommand.cs
+++ src/CadCommand.cs
@@ -1,8 +1,9 @@
-#if NC
-//using HostMgd.ApplicationServices;
-//using HostMgd.EditorInput;
+using System.ComponentModel;
+using System.Reflection;
 
-//using Teigha.DatabaseServices;
+using drz.Infrastructure.Service;
+
+#if NC
 
 using App = HostMgd.ApplicationServices;
 using Db = Teigha.DatabaseServices;
@@ -10,19 +11,10 @@ using Ed = HostMgd.EditorInput;
 using Rtm = Teigha.Runtime;
 
 #elif AC
-
-//using Autodesk.AutoCAD.ApplicationServices;
-//using Autodesk.AutoCAD.DatabaseServices;
-//using Autodesk.AutoCAD.EditorInput;
-//using Autodesk.AutoCAD.Windows;
-
 using App = Autodesk.AutoCAD.ApplicationServices;
-//using Cad = Autodesk.AutoCAD.ApplicationServices.Application;
 using Db = Autodesk.AutoCAD.DatabaseServices;
 using Ed = Autodesk.AutoCAD.EditorInput;
-//using Gem = Autodesk.AutoCAD.Geometry;
 using Rtm = Autodesk.AutoCAD.Runtime;
-
 
 #endif
 [assembly: Rtm.CommandClass(typeof(drz.ChangeDBmod.CadCommand))]
@@ -34,26 +26,12 @@ namespace drz.ChangeDBmod
     /// </summary>
     class CadCommand : Rtm.IExtensionApplication
     {
-
-        #region INIT
+                #region INIT
         public void Initialize()
         {
-            //think добавить проверку есть ли doc
-
-            App.DocumentCollection dm = App.Application.DocumentManager;
-
-
-            Ed.Editor ed = dm.MdiActiveDocument.Editor;
-            //!+Вывожу список команд определенных в библиотеке
-            ed.WriteMessage("\nStart list of commands: \n");
-            string sCom =
-                "drz_changedb" + "\tПереключатель баз MultiCad";
-            ed.WriteMessage(sCom);
-            //ed.WriteMessage("End list of commands\n");
-
-#if DEBUG
-            //для отладки список команд, чтоб лишнего не попало
-#endif           
+            //выводим список команд с описаниями
+            CmdInfo comInf = new CmdInfo();
+            comInf.Reflection(); //модуль в этой сборке
         }
 
         public void Terminate()
@@ -66,25 +44,26 @@ namespace drz.ChangeDBmod
         #region Command
 
         /// <summary>
-        /// переключалка нанобаз
+        /// Переключатель баз MultiCad
         /// </summary>
         [Rtm.CommandMethod("drz_changedb", Rtm.CommandFlags.Session)]
+        [Description("Переключатель баз MultiCad")]
         public void changedbMod()
         {
-            Db.Database db = Db.HostApplicationServices.WorkingDatabase;
             App.Document doc = App.Application.DocumentManager.MdiActiveDocument;
+            Db.Database db = doc.Database;
             Ed.Editor ed = doc.Editor;
 
             Ed.PromptStringOptions opts = new Ed.PromptStringOptions("enter base:");
             opts.AllowSpaces = true;
             Ed.PromptResult pr = ed.GetString(opts);
+
             if (Ed.PromptStatus.OK == pr.Status)
             {
                 bool bSetBD = Multicad.AplicationServices.McParamManager.SetParam(pr.StringResult, 9);
-
             }
-            //return;
-            //string oldBd = Multicad.AplicationServices.McParamManager.GetStringParam(9);//получаем путь к базе текущего приложения
+            //Example switch other database;
+            //string oldBd = Multicad.AplicationServices.McParamManager.GetStringParam(9);//получаем путь свойства базы текущего приложения
 
             //string sMDF = "z:\\BD_SQL\\nana\\std.mdf";//local *.mdf
             //bool bsetBD = Multicad.AplicationServices.McParamManager.SetParam(sMDF, 9);
@@ -96,7 +75,6 @@ namespace drz.ChangeDBmod
             //bsetBD = Multicad.AplicationServices.McParamManager.SetParam(sPSQL, 9);
 
         }
-
 
         #endregion
     }
