c9a407fc7e29c827e693e2f72595df344dadb4c7
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2026-02-02 13:52:48 +0300

    бред жопите

---
+++ src/Servise/McParamManagerProxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+/// <summary>
+/// Идея жопите, нерабочая
+/// </summary>
+static class McParamManagerProxy
+{
+    private static dynamic _impl;
+
+    static McParamManagerProxy()
+    {
+        _impl = FindMcParamManager()
+            ?? throw new InvalidOperationException("McParamManager не найден");
+    }
+
+    private static Type FindMcParamManager()
+    {
+        foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            Type type = asm
+                .GetTypes()
+                .FirstOrDefault(t =>
+                    t.Name == "McParamManager" &&
+                    t.GetMethod("SetParam", new[] { typeof(string), typeof(int) }) != null);
+
+            if (type != null)
+                return type;
+        }
+
+        return null;
+    }
+
+    public static bool SetParam(string value, int index)
+        => _impl.SetParam(value, index);
+
+    public static string GetStringParam(int index)
+        => _impl.GetStringParam(index);
+}
